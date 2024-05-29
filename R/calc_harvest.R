86c66072114672b68e939e7d7a07e20ba0d81033
bwsimmons541 <83658254+bwsimmons541@users.noreply.github.com>
2024-05-29 16:38:05 -0700

    Updated and cleaned up calc_harvest function to accurately calculate sport and tribal harvest per references from Jack Yearout (NPT) & Kyle Bratcher (ODFW)

--- R/calc_harvest.R
+++ R/calc_harvest.R
@@ -2,15 +2,17 @@
 #' Calculations were originally copied from Kyle Bratcher's R script and Jack Yearout's
 #' spreadsheet.
 #'
-#' Author Ryan Kinzer
+#' Authors Ryan Kinzer & Brian Simmons
 #' Created May 10, 2024
 #' 
 #'
 
 
-#nor_esc = 326
-#hor_esc = 576
-#broodstock = 163
+nor_esc = 326 # 2024 pre season estimate
+hor_esc = 576 # 2024 pre-season estimate
+broodstock = 164
+
+# calc_harvest(nor_esc = nor_esc, hor_esc = hor_esc, broodstock = broodstock)
 
 calc_harvest <- function(nor_esc, hor_esc, broodstock){
   # ADULT BROODSTOCK NEEDS FROM AOP ---- 
@@ -22,144 +24,162 @@ calc_harvest <- function(nor_esc, hor_esc, broodstock){
   
   # Forecast natural origin (NO) adults for Lostine River (LST)
   
-    lst_no <- nor_esc
+    lst_NO <- nor_esc
   
   # Forecast hatchery origin (HO) adults for Lostine River (LST)
   
-    lst_ho <- hor_esc
+    lst_HO <- hor_esc
   
   # Wallowa/Lostine (WAL_LST) expansion natural origin (NO) adults
   
   # Forecast total NO WAL/LST ****"1.4" is the expansion used based on redd counts within the basin. 
   # ****This may require updating in the future.****
   
-  wal_lst_no<-round(lst_no*1.4,0)
+  wal_lst_NO<-round(lst_NO * 1.4,0)
   
   # Allowed Natural Origin (NO) Impacts ----
   
-  # Allowed NO Sportfish Impacts Wallowa/Lostine (WAL_LST) ----
+# Allowed NO Sportfish Impacts Wallowa/Lostine (WAL_LST) ----
   # References harvest scale
-  if(wal_lst_no<300){
-    wal_lst_sprt_impct<-0
-  }else{
-    if(wal_lst_no<1000) {
-      wal_lst_sprt_impct<-round((wal_lst_no-300)*.03,0)
+    
+    if(wal_lst_NO<300){
+      sprt_NO_impact<-0
     }else{
-      if(wal_lst_no<1500){
-        wal_lst_sprt_impct<-round(21+(1499-999)*.06,0)
+      if(wal_lst_NO<1000) {
+        sprt_NO_impact<-round((wal_lst_NO-300)*.03,0)
       }else{
-        if(wal_lst_no<2000){
-          wal_lst_sprt_impct<-round(51+(1999-1500)*.06,0)
+        if(wal_lst_NO<1500){
+          sprt_NO_impact<-round(21+(1499-999)*.06,0)
         }else{
-          if(wal_lst_no>1999){
-            wal_lst_sprt_impct<-round(81+(wal_lst_no-1500)*.12,0)}
-        }
-      }}}
-  
-  # wal_lst_sprt_impct = the number of allowed NOR take from incidental mortality
-  
-  # Sport Handle Rate - Wallowa/Lostine (WAL_LST) - assumes a 10% mortality on all caught NOR fish
-  wal_lst_no_hndl<-wal_lst_sprt_impct*10
-  
-  # allowed wild impact from FMEP - from Jack and it doesn't match Kyle's above
-  # if(((wal_lst_no - 300)*.03)<0){
-  #   wild_impact <- 0
-  # } else {
-  #   wild_impact <- round(((wal_lst_no - 300)*.03),0)
-  # }
-  # 
-  # impact_rate <- wild_impact/wal_lst_no
-  # wild_hnd <- wild_impact*10   # same as wal_lst_no_hndl
-  
-  # Sport Harvest ----
-  # from Kyle
-  
-  HOsprt_hvst<-round((wal_lst_no_hndl/wal_lst_no)*lst_ho) # what does this mean?????? if we catch the same proportion of HOR as we can catch for NOR and stay under the take limit, this is the number of HOR fish we can catch/keep, doesn't account for brood needs
-  
-  # Allowed Tribal natural origin (NO) Impacts (WAL_LST) ----
-  
-  #References harvest scale - does not match Jack's
-  if(wal_lst_no<225){
-    lst_trb_impct<-round(wal_lst_no*.01)
-  }else{
-    if(wal_lst_no<750){
-      lst_trb_impct<-round(4.5+(lst_no-225)*.08,0)
-    }else{
-      if(wal_lst_no<1125){
-        lst_trb_impct<-round(47.25+(lst_no-750)*.16,0)
-      }else{
-        if(wal_lst_no<1500){
-          lst_trb_impct<-round(114.75+(lst_no-1125)*.19,0)
-        }else{
-          if(wal_lst_no>1499){
-            lst_trb_impct<-round(114.75+(lst_no-1500)*.28,0)}
-        }
-      }}}
-  
-  # used in JCAPE analysis but they don't match Jack's spreadsheet
-  # pMAT_upper <- c(.3, .5, .75, 1.08)
-  # NPT_harvest <- c(.01, .02, .03, .044, .175)
-  # harvest_thres <- viable * pMAT_upper
-  
-  # tribal harvest sliding scale
-  if(lst_ho < 300){
-    treaty_hor_harvest <- lst_ho * .02
-  } else {
-    if(wal_lst_no < 1000){
-      treaty_hor_harvest <- 6 + ((lst_ho - 300) * .09)
-    } else {
-      if(wal_lst_no < 1500){
-        treaty_hor_harvest <- 69 + ((lst_ho - 1000) * .18)
-      } else {
-        if(wal_lst_no < 2000){
-          treaty_hor_harvest <- 159 + ((lst_ho - 1500) * .21)
-        } else {
-          if(wal_lst_no >= 2000){
-            treaty_hor_harvest <- 264 + ((lst_ho - 1999) * .4)
-          } else {
-            treaty_hor_harvest <- 0
+          if(wal_lst_NO<2000){
+            sprt_NO_impact<-round(51+(1999-1500)*.06,0)
+          }else{
+            if(wal_lst_NO>1999){
+              sprt_NO_impact<-round(81+(wal_lst_NO-1500)*.12,0)}
           }
-        }
-      }
-    }
-  }
+        }}}
+    
+    # sprt_NO_impact = the number of allowed NOR take from incidental mortality
+    
+    # Sport Handle Rate - Wallowa/Lostine (WAL_LST) - assumes a 10% mortality on all caught NOR fish
+    
+    wal_lst_NO_hndl<-sprt_NO_impact*10 # This might not be necessary for our purposes.
+    
+    # allowed wild impact from FMEP - from Jack and it doesn't match Kyle's above
+    # if(((wal_lst_NO - 300)*.03)<0){
+    #   wild_impact <- 0
+    # } else {
+    #   wild_impact <- round(((wal_lst_NO - 300)*.03),0)
+    # }
+    # 
+    # impact_rate <- wild_impact/wal_lst_NO
+    # wild_hnd <- wild_impact*10   # same as wal_lst_NO_hndl
   
-  treaty_hor_harvest <- round(treaty_hor_harvest, 0)
+# Sport Hatchery Origin Harvest ----
   
-  if(wal_lst_no < 300){
-    treaty_nor_harvest <- wal_lst_no * .01
-  } else {
-    if(wal_lst_no < 1000){
-      treaty_nor_harvest <- 3 + ((wal_lst_no - 300) * .08)
-    } else {
-      if(wal_lst_no < 1500){
-        treaty_nor_harvest <- 59 + ((wal_lst_no - 1000) * .16)
-      } else {
-        if(wal_lst_no < 2000){
-          treaty_nor_harvest <- 139 + ((wal_lst_no - 1500) * .19)
-        } else {
-          if(wal_lst_no >= 2000){
-            treaty_nor_harvest <- 234 + ((wal_lst_no - 1999) * .28)
-          } else {
-            treaty_nor_harvest <- 0
-          }
-        }
-      }
-    }
-  }
+    sprt_HO_hvst<-round((wal_lst_NO_hndl/wal_lst_NO)*lst_HO) 
   
-  treaty_nor_harvest <- round(treaty_nor_harvest, 0)
-  #treaty_hor_harvest <- (wild_impact + HOsprt_hvst) - treaty_harvest
+    # ODFW assumes that the catch rate is equal for Natural Origin and Hatchery Origin Fish
+    # ODFW sets sport harvest as the proportion of NO handled /  NO return * the Hatchery Origin Return
+    # This theoretically sets the maximum HO harvest limit and stays at or under NO impact
+  
+  
 
-  # Hatchery Origin (HO) Harvest Shares ---- 
-  # !!!!THESE ARE PRELIMINARY ESTIMATES. DO NOT USE AS FINAL HARVEST SHARES!!!!----
+# Tribal Natural Origin Impact ----
   
+    # This Calculation Matches Jack's Spreadsheet
   
-  # lst_ho - lst_bs_need
-  # HOsprt_hvst + treaty_hor_harvest
+        if(wal_lst_NO < 300){
+          treaty_NO_impact <- wal_lst_NO * .01
+        } else {
+          if(wal_lst_NO < 1000){
+            treaty_NO_impact <- 3 + ((wal_lst_NO - 300) * .08)
+          } else {
+            if(wal_lst_NO < 1500){
+              treaty_NO_impact <- 59 + ((wal_lst_NO - 1000) * .16)
+            } else {
+              if(wal_lst_NO < 2000){
+                treaty_NO_impact <- 139 + ((wal_lst_NO - 1500) * .19)
+              } else {
+                if(wal_lst_NO >= 2000){
+                  treaty_NO_impact <- 234 + ((wal_lst_NO - 1999) * .28)
+                } else {
+                  treaty_NO_impact <- 0
+                }
+              }
+            }
+          }
+        }
+
+        treaty_NO_impact <- round(treaty_NO_impact, 0)
+
+    # This calculation matches ODFW the difference will impact TOTAL NO impact
+    # 
+        
+        # if(wal_lst_NO < 225){
+        #   treaty_NO_impact <- round(wal_lst_NO * .01)
+        # }else{
+        #   if(wal_lst_NO < 750){
+        #     treaty_NO_impact <- round(4.5 + (lst_NO - 225) * .08, 0)
+        #   }else{
+        #     if(wal_lst_NO < 1125){
+        #       treaty_NO_impact <- round(47.25 + (lst_NO - 750) * .16, 0)
+        #     }else{
+        #       if(wal_lst_NO < 1500){
+        #         treaty_NO_impact <- round(114.75 + (lst_NO - 1125) * .19, 0)
+        #       }else{
+        #         if(wal_lst_NO > 1499){
+        #           treaty_NO_impact <- round(114.75 + (lst_NO - 1500) * .28, 0)}
+        #       }
+        #     }
+        #   }
+        # }
+        # 
+        # treaty_NO_impact <- round(treaty_NO_impact, 0)
+      
+      
+          
+        
+# Tribal Hatchery Origin Harvest ----
+      
+      #Assumes that ODFW correctly calculates TOTAL harvest share and NPT share is equivalent.
   
-  nor_harvest = wal_lst_sprt_impct + treaty_nor_harvest #should include the NOR incidental mortality impact of sport harvest and the number of tribal harvest
-  hor_harvest = HOsprt_hvst + treaty_hor_harvest #is this only a 50% share, should it be doubled to include the sport harvest share plus the tribal harvest share
+      treaty_HO_hvst <- (sprt_HO_hvst + sprt_NO_impact) - treaty_NO_impact
+      
+  
+      # tribal harvest sliding scale
+      # this matches NPT long-term Imnaha/Grande Ronde harvest plan
+      # Does NOT match ODFW NPT harvest share agreement
+      # 
+      # if(lst_HO < 300){
+      #   treaty_HO_hvst <- lst_HO * .02
+      # } else {
+      #   if(wal_lst_NO < 1000){
+      #     treaty_HO_hvst <- 6 + ((lst_HO - 300) * .09)
+      #   } else {
+      #     if(wal_lst_NO < 1500){
+      #       treaty_HO_hvst <- 69 + ((lst_HO - 1000) * .18)
+      #     } else {
+      #       if(wal_lst_NO < 2000){
+      #         treaty_HO_hvst <- 159 + ((lst_HO - 1500) * .21)
+      #       } else {
+      #         if(wal_lst_NO >= 2000){
+      #           treaty_HO_hvst <- 264 + ((lst_HO - 1999) * .4)
+      #         } else {
+      #           treaty_HO_hvst <- 0
+      #         }
+      #       }
+      #     }
+      #   }
+      # }
+      # 
+      #   treaty_HO_hvst <- round(treaty_HO_hvst, 0)
+
+
+# Total harvest by origin ----
+  
+  nor_harvest = sprt_NO_impact + treaty_NO_impact # includes the NOR incidental mortality impact of sport harvest and the number of tribal harvest
+  hor_harvest = sprt_HO_hvst + treaty_HO_hvst 
 
     return(list('nor_harvest' = nor_harvest, 'hor_harvest' = hor_harvest))
 }
